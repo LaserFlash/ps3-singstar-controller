801ba6688daf4cc2ad6ce03a9c88b81af9ae64fd
Bryn Bennett <bryn.t.bennett@gmail.com>
2025-11-30 11:11:29 +1300

    initial setup

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align="center">
+
+![PS3 Singstar controller logo](public/logo192.png)
+
+## PS3 Singstar Controller
+
+`webman` `singstar ps2` `singstar ps3`
+
+</div>
+
+# Development
+
+To run this application:
+
+```bash
+npm install
+npm run start
+```
+
+# Building For Production
+
+To build this application for production:
+
+```bash
+npm run build
+```
+
+## Testing
+
+This project uses [Vitest](https://vitest.dev/) for testing. You can run the tests with:
+
+```bash
+npm run test
+```
+
+## Linting & Formatting
+
+This project uses [eslint](https://eslint.org/) and [prettier](https://prettier.io/) for linting and formatting. Eslint is configured using [tanstack/eslint-config](https://tanstack.com/config/latest/docs/eslint). The following scripts are available:
+
+```bash
+npm run lint
+npm run format
+npm run check
+```
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    viteSingleFile(),
+    {
+      name: 'rename',
+      enforce: 'post',
+      generateBundle(_, bundle) {
+        bundle['index.html'].fileName = bundle['index.html'].fileName.replace('index', 'singstar');
+      },
+    },
+  ],
+  resolve: { alias: { '@': resolve(__dirname, './src') } },
+  build: { target: 'baseline-widely-available' },
+});
